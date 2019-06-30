91edc25dc63cf2cb09217543e440e90cb0e6c237
vishnumalar <mail2kvishnu@gmail.com>
2019-06-30 13:36:33 +0530

    webhook

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -2,9 +2,10 @@ public class AddTwoNumbers {
 
    public static void main(String[] args) {
         
-      int num1 = 5, num2 = 15, sum;
-      sum = num1 + num2;
+      int num1 = 5, num2 = 15, num3 =10, sum;
+      sum = num1 + num2 + num3;
 
-      System.out.println("Sum of these numbers: "+sum);
-   }
+System.out.println("Sum of these numbers: "+sum);
+
+      System.out.println("completed")}
 }
